53b131d44ae5c8c1a43c55e3d3450f6337ff946e
krithikakannan751 <90995897+krithikakannan751@users.noreply.github.com>
2021-09-21 10:25:38 -0500

    Add files via upload

---
+++ Palindrome Checker/Lab14atst.java
@@ -0,0 +1,41 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class Lab14atst
+{
+	public static void main ( String[] args )
+	{
+		
+		String data = "one two three two one";
+		PalinList test = new PalinList(data);
+		System.out.println (test);
+		
+		data = "1 2 3 4 5 one two three four five";
+		test.setList(data);
+		test.toString();
+		
+		data = "a b c d e f g x y z g f h";
+		test.setList(data);
+		test.toString();
+		
+		data = "racecar is racecar";
+		test.setList(data);
+		test.toString();
+		
+		data = "1 2 3 a b c c b a 3 2 1";
+		test.setList(data);
+		test.toString();
+		
+		data = "chicken is a chicken";
+		test.setList(data);
+		test.toString();
+		
+		data = "racecar $ $ racecar";
+		test.setList(data);
+		test.toString();
+		System.out.println ("END\n");		
+	}
+}
---
+++ Palindrome Checker/PalinList.java
@@ -0,0 +1,63 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+import java.util.*;
+
+public class PalinList
+{
+	private Queue<String> queue;
+	private Stack<String> stack;
+	private ArrayList<String> print;
+
+	public PalinList()
+	{
+		setList("");
+	}
+
+	public PalinList(String list)
+	{
+
+		queue = new LinkedList<String>();
+		stack = new Stack<String>();
+		print = new ArrayList<String>();
+		setList(list);
+	}
+
+	public void setList(String list)
+	{
+	
+		String[] str = list.split(" ");
+		queue.clear();
+		stack.clear();
+		print.clear();
+		for(String s: str)
+		{
+			queue.add(s);
+			stack.add(s);
+			print.add(s);
+		}
+	}
+
+	public boolean isPalin()
+	{
+		while(!queue.isEmpty() && !stack.isEmpty())
+		{
+			
+			if(queue.remove().equals(stack.pop()))
+				return true;
+		}
+		return false;
+	}
+
+	
+	//write a toString method
+	public String toString()
+	{
+		String s = "";
+		if(!isPalin())
+			s = "not ";
+		return print + " is " + s + " a palinlist.";
+	}
+}
