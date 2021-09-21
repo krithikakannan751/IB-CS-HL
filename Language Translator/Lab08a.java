a3b5d6a1c1e7087f5401151a1b903d9ba517cb97
krithikakannan751 <90995897+krithikakannan751@users.noreply.github.com>
2021-09-21 11:35:11 -0500

    Add files via upload

---
+++ Language Translator/Lab08a.java
@@ -0,0 +1,38 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Lab  -
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+import static java.lang.System.*;
+
+public class Lab08a
+{
+	public static void main( String args[] ) throws IOException
+	{
+		Scanner file = new Scanner(new File("lab08a.dat"));
+
+		SpanishToEnglish test = new SpanishToEnglish();
+
+
+		//read in pairs from the file and load the map
+
+
+		out.println("\n====\tMAP CONTENTS\t====\n\n");
+
+		//print the map
+
+
+		out.println(test+"\n\n");
+
+
+		//read in the lines and convert them
+
+
+
+
+
+	}
+}
