248acc2bd113da091b3567ee6667779448da8c93
krithikakannan751 <90995897+krithikakannan751@users.noreply.github.com>
2021-09-21 11:39:51 -0500

    Add files via upload

---
+++ Maps Examples/TreeMapOutputNew.java
@@ -0,0 +1,36 @@
+
+//printing out a map with the new for loop
+
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.Iterator;
+
+public class TreeMapOutputNew
+{
+	public static void main(String args[])
+	{
+		Map<Character,Integer> map;
+		map = new TreeMap<Character,Integer>();
+
+		String s = "cabcdefghihabcdc";
+		for(char c : s.toCharArray())
+		{
+			if(map.get(c)==null)
+			{
+				map.put(c,1);
+			}
+			else
+			{
+			   map.put(c,map.get(c)+1);
+			}
+		}
+
+		for(char c : map.keySet())
+		{
+			System.out.println(c+" - "+map.get(c));
+		}
+		
+		System.out.println(map);
+		
+	}
+}
